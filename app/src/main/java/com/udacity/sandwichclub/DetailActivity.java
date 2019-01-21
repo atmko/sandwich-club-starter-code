748486aba33f24fb0cfbb40a4a52c217ba1676c2
sigmko <44901543+sigmko@users.noreply.github.com>
2019-01-21 11:18:36 -0500

    Add UI organization
    JsonUtils.java
        Add convertJSONArray to List method
        Complete parseSandwichJson method
    
    DetailActivity.java
        Add fields and variables for UI (elements) i.e Views
        Complete populateUI method
        Add helper functions: populateAlsoKnownAs and populateIngredients
    
    activity_detail.xml
        Add scrollView to fit content
        Match information TextViews to proper TextView header
        Add Layout_margin

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    TextView mAlso_known_tv;
+    TextView mIngredients_tv;
+    TextView mOrigin_tv;
+    TextView mDescription_tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mAlso_known_tv = findViewById(R.id.also_known_tv);
+        mIngredients_tv = findViewById(R.id.ingredients_tv);
+        mOrigin_tv = findViewById(R.id.origin_tv);
+        mDescription_tv = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,6 +47,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
+        Log.i("JSON", json);
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
@@ -43,7 +55,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,42 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateAlsoKnownAs(Sandwich sandwich) {
+        int akaLength = sandwich.getAlsoKnownAs().size();
+
+        //iterate through alsoKnownAs list
+        for (String currentAka : sandwich.getAlsoKnownAs()) {
+            mAlso_known_tv.append(currentAka);
+            //if alsoKnownAs list not empty and the currentAka is not the last then add ", "
+            //checking akaLength!=0 prevents "-1" value when checking if currentAka is last
+            if (akaLength!=0 && !(sandwich.getAlsoKnownAs().get(akaLength -1).equals(currentAka))) {
+                mAlso_known_tv.append(", ");
+
+            }
+        }
+    }
+    private void populateIngredients(Sandwich sandwich) {
+        int ingredientLength = sandwich.getIngredients().size();
+
+        //iterate through ingredients list
+        for (String currentIngredient : sandwich.getIngredients()) {
+            mIngredients_tv.append("-" + currentIngredient);
+            //if the the currentIngredient is not the last then add "\n"
+            if (!(sandwich.getIngredients()).get(ingredientLength -1).equals(currentIngredient)) {
+                mIngredients_tv.append("\n");
+            }
+
+        }
+
+    }
+
+    private void populateUI(Sandwich sandwich) {
+        //additional logic needed for populating also known as and ingredient views
+        populateAlsoKnownAs(sandwich);
+        populateIngredients(sandwich);
+        //no additional complex logic needed for populating remaining views
+        mOrigin_tv.setText(sandwich.getPlaceOfOrigin());
+        mDescription_tv.setText(sandwich.getDescription());
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,58 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject name = jsonObject.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            //--------
+            JSONArray alsoKnownAsRaw = name.getJSONArray("alsoKnownAs");
+            //convert JSONArray to list
+            List<String> alsoKnownAs = convertJSONArrayToList(alsoKnownAsRaw);
+            //--------
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+            //--------
+            JSONArray ingredientsRaw = jsonObject.getJSONArray("ingredients");
+            //convert JSONArray to list
+            List<String> ingredients = convertJSONArrayToList(ingredientsRaw);
+            //--------
+            //return sandwich
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static List<String> convertJSONArrayToList(JSONArray jsonArray) {
+        List<String> list = new ArrayList<>();
+        int length = jsonArray.length();
+
+        //iterate through JSONArray
+        for (int position = 0 ; position < length ; position++) {
+            try {
+                //add items individually to list
+                list.add((String) jsonArray.get(position));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return list;
     }
 }
